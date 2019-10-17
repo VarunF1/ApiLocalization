9e9610e30f6126bfd65951ce860bc5c3c94cae6f
H305875 <varun.sood@honeywell.com>
2019-10-17 17:00:33 +0530

    First commit

---
+++ Api/Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Api.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<MyPayload> Get()
+        {
+            var myPayLoad = new MyPayload
+            {
+                Description = Resources.AmazingResource.Description,
+                Timestamp = DateTime.UtcNow,
+                Name = Resources.AmazingResource.Name
+            };
+            return new[] { myPayLoad };
+        }
+
+        //// GET api/values
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public IHttpActionResult Post([FromBody]MyPayload valuevalue)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok();
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
